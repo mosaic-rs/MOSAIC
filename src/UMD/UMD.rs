5026d30877f11ab9b7e9ae5d8e7815b2e72d59a1
harrywoodhouse <woodhouses.harry@gmail.com>
2026-01-21 01:27:17 +0000

    UMD Substruct uncertainty implementation

--- src/UMD/UMD.rs
+++ src/UMD/UMD.rs
@@ -136,54 +136,54 @@ impl UMD {
 
     }
 
-    pub fn add_point(&mut self, raw: &UMDDriver, anchor: &UMDAnchor, centered: &UMDCenter, rotated: &UMDPose) {
+    pub fn add_point(&mut self, raw: &UMDDriver, anchor: &UMDAnchor, centered: &UMDCentered, rotated: &UMDPose) {
         
-            self.frame.push(raw.frame);
-            self.timestamp.push(raw.timestamp);
-            self.confidence.push(raw.confidence); // needs adding
-            self.pose.push(raw.pose);
-            self.coordinate_number.push(raw.coordinate_number);
-            self.types.push(raw.types);
+            self.frame = (raw.frame);
+            self.timestamp = (raw.timestamp);
+            self.confidence = (raw.confidence); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.pose = (raw.pose);
+            self.coordinate_number = (raw.coordinate_number);
+            self.types = (raw.types);
 
             // pose
-            self.pose_x.push(raw.pose_x);
-            self.pose_y.push(raw.pose_y);
-            self.pose_z.push(raw.pose_z);
-            self.pose_x_uncertainty.push(raw.pose_x_uncertainty); // needs adding
-            self.pose_y_uncertainty.push(raw.pose_y_uncertainty); // needs adding
-            self.pose_z_uncertainty.push(raw.pose_z_uncertainty); // needs adding
+            self.pose_x = (raw.pose_x);
+            self.pose_y = (raw.pose_y);
+            self.pose_z = (raw.pose_z);
+            self.pose_x_uncertainty = (raw.pose_x_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.pose_y_uncertainty = (raw.pose_y_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.pose_z_uncertainty = (raw.pose_z_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
 
             // raw coordinates
-            self.x_raw.push(raw.x);
-            self.y_raw.push(raw.y);
-            self.z_raw.push(raw.z);
-            self.x_raw_uncertainty.push(raw.x_uncertainty); // needs adding
-            self.y_raw_uncertainty.push(raw.y_uncertainty); // needs adding
-            self.z_raw_uncertainty.push(raw.z_uncertainty); // needs adding
+            self.x_raw = (raw.x);
+            self.y_raw = (raw.y);
+            self.z_raw = (raw.z);
+            self.x_raw_uncertainty = (raw.x_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.y_raw_uncertainty = (raw.y_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.z_raw_uncertainty = (raw.z_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
 
             // centered coordinates
-            self.x_centered.push(centered.x);
-            self.y_centered.push(centered.y);
-            self.z_centered.push(centered.z);
-            self.x_centered_uncertainty.push(centered.x_uncertainty); // needs adding
-            self.y_centered_uncertainty.push(centered.y_uncertainty); // needs adding
-            self.z_centered_uncertainty.push(centered.z_uncertainty); // needs adding
+            self.x_centered = (centered.x);
+            self.y_centered = (centered.y);
+            self.z_centered = (centered.z);
+            self.x_centered_uncertainty = (centered.x_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.y_centered_uncertainty = (centered.y_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.z_centered_uncertainty = (centered.z_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
 
             // pose corrected coordinates
-            self.x_rotated.push(rotated.x);
-            self.y_rotated.push(rotated.y);
-            self.z_rotated.push(rotated.z);
-            self.x_rotated_uncertainty.push(rotated.x_uncertainty); // needs adding
-            self.y_rotated_uncertainty.push(rotated.y_uncertainty); // needs adding
-            self.z_rotated_uncertainty.push(rotated.z_uncertainty); // needs adding
+            self.x_rotated = (rotated.x);
+            self.y_rotated = (rotated.y);
+            self.z_rotated = (rotated.z);
+            self.x_rotated_uncertainty = (rotated.x_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.y_rotated_uncertainty = (rotated.y_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.z_rotated_uncertainty = (rotated.z_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
 
             // anchor
-            self.x_anchor.push(anchor.x_anchor);
-            self.y_anchor.push(anchor.y_anchor);
-            self.z_anchor.push(anchor.z_anchor);
-            self.x_anchor_uncertainty.push(anchor.x_anchor_uncertainty); // needs adding
-            self.y_anchor_uncertainty.push(anchor.y_anchor_uncertainty); // needs adding
-            self.z_anchor_uncertainty.push(anchor.z_anchor_uncertainty); // needs adding
+            self.x_anchor = (anchor.x_anchor);
+            self.y_anchor = (anchor.y_anchor);
+            self.z_anchor = (anchor.z_anchor);
+            self.x_anchor_uncertainty = (anchor.x_anchor_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.y_anchor_uncertainty = (anchor.y_anchor_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+            self.z_anchor_uncertainty = (anchor.z_anchor_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
     }
 
     pub fn save_umd_driver_to_parquet(data: &UMD, file_path: &str) -> PolarsResult<()> {
@@ -197,30 +197,30 @@ impl UMD {
         let s_pose_x = Series::new("pose_x", &data.pose_x);
         let s_pose_y = Series::new("pose_y", &data.pose_y);
         let s_pose_z = Series::new("pose_z", &data.pose_z);
-        let s_pose_x_uncertainty = Series::new("pose_x_uncertainty", &data.pose_x_uncertainty); // needs adding
-        let s_pose_y_uncertainty = Series::new("pose_y_uncertainty", &data.pose_y_uncertainty); // needs adding
-        let s_pose_z_uncertainty = Series::new("pose_z_uncertainty", &data.pose_z_uncertainty); // needs adding
+        let s_pose_x_uncertainty = Series::new("pose_x_uncertainty", &data.pose_x_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+        let s_pose_y_uncertainty = Series::new("pose_y_uncertainty", &data.pose_y_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+        let s_pose_z_uncertainty = Series::new("pose_z_uncertainty", &data.pose_z_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
 
         let s_x_raw = Series::new("x_raw", &data.x_raw);
         let s_y_raw = Series::new("y_raw", &data.y_raw);
         let s_z_raw = Series::new("z_raw", &data.z_raw);
-        let s_x_raw_uncertainty = Series::new("x_raw_uncertainty", &data.x_raw_uncertainty); // needs adding
-        let s_y_raw_uncertainty = Series::new("y_raw_uncertainty", &data.y_raw_uncertainty); // needs adding
-        let s_z_raw_uncertainty = Series::new("z_raw_uncertainty", &data.z_raw_uncertainty); // needs adding
+        let s_x_raw_uncertainty = Series::new("x_raw_uncertainty", &data.x_raw_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+        let s_y_raw_uncertainty = Series::new("y_raw_uncertainty", &data.y_raw_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+        let s_z_raw_uncertainty = Series::new("z_raw_uncertainty", &data.z_raw_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
 
         let s_x_centered = Series::new("x_centered", &data.x_centered);
         let s_y_centered = Series::new("y_centered", &data.y_centered);
         let s_z_centered = Series::new("z_centered", &data.z_centered);
-        let s_x_centered_uncertainty = Series::new("x_centered_uncertainty", &data.x_centered_uncertainty); // needs adding
-        let s_y_centered_uncertainty = Series::new("y_centered_uncertainty", &data.y_centered_uncertainty); // needs adding
-        let s_z_centered_uncertainty = Series::new("z_centered_uncertainty", &data.z_centered_uncertainty); // needs adding
+        let s_x_centered_uncertainty = Series::new("x_centered_uncertainty", &data.x_centered_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+        let s_y_centered_uncertainty = Series::new("y_centered_uncertainty", &data.y_centered_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+        let s_z_centered_uncertainty = Series::new("z_centered_uncertainty", &data.z_centered_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
 
         let s_x_rotated = Series::new("x_rotated", &data.x_rotated);
         let s_y_rotated = Series::new("y_rotated", &data.y_rotated);
         let s_z_rotated = Series::new("z_rotated", &data.z_rotated);
-        let s_x_rotated_uncertainty = Series::new("x_rotated_uncertainty", &data.x_rotated_uncertainty); // needs adding
-        let s_y_rotated_uncertainty = Series::new("y_rotated_uncertainty", &data.y_rotated_uncertainty); // needs adding
-        let s_z_rotated_uncertainty = Series::new("z_rotated_uncertainty", &data.z_rotated_uncertainty); // needs adding
+        let s_x_rotated_uncertainty = Series::new("x_rotated_uncertainty", &data.x_rotated_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+        let s_y_rotated_uncertainty = Series::new("y_rotated_uncertainty", &data.y_rotated_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
+        let s_z_rotated_uncertainty = Series::new("z_rotated_uncertainty", &data.z_rotated_uncertainty); // needs adding - IMPLEMENTED IN SUBSCTRUCT
 
         let s_x_anchor = Series::new("x_anchor", &data.x_anchor);
         let s_y_anchor = Series::new("y_anchor", &data.y_anchor);
@@ -276,6 +276,11 @@ pub struct UMDDriver {
     pub x: Vec<f64>,
     pub y: Vec<f64>,
     pub z: Vec<f64>,
+    pub x_raw_uncertainty: Vec<f64>,
+    pub y_raw_uncertainty: Vec<f64>,
+    pub z_raw_uncertainty: Vec<f64>,
+
+
 }
 
 impl UMDDriver{
@@ -290,16 +295,19 @@ impl UMDDriver{
             pose_x: Vec::with_capacity(total_entries.try_into().unwrap()),
             pose_y: Vec::with_capacity(total_entries.try_into().unwrap()),
             pose_z: Vec::with_capacity(total_entries.try_into().unwrap()),
-            coordinate_number: Vec::with_capacity(total_entries.try_into().unwrap()),
             types: Vec::with_capacity(total_entries.try_into().unwrap()),
             x: Vec::with_capacity(total_entries.try_into().unwrap()),
             y: Vec::with_capacity(total_entries.try_into().unwrap()),
             z: Vec::with_capacity(total_entries.try_into().unwrap()),
+            x_raw_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()),
+            y_raw_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()),
+            z_raw_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()),
         }
 
     }
 
-    pub fn add_point(&mut self, frame: u32, time: f32, confidence: f32, pose: bool, pose_x: f64, pose_y: f64, pose_z: f64, number: u32, types: String, x: f64, y: f64, z: f64) {
+    pub fn add_point(&mut self, frame: u32, time: f32, confidence: f32, pose: bool, pose_x: f64, pose_y: f64, pose_z: f64, 
+                     number: u32, types: String, x: f64, y: f64, z: f64, x_raw_uncertainty: f64, y_raw_uncertainty: f64, z_raw_uncertainty: f64) {
         
         self.frame.push(frame);
         self.timestamp.push(time);
@@ -312,6 +320,9 @@ impl UMDDriver{
         self.x.push(x);
         self.z.push(z);
         self.y.push(y);
+        self.x_raw_uncertainty.push(x_raw_uncertainty);
+        self.y_raw_uncertainty.push(y_raw_uncertainty);
+        self.z_raw_uncertainty.push(z_raw_uncertainty);
         
     }
 
@@ -333,11 +344,16 @@ impl UMDDriver{
         let s_y = Series::new("y_raw", &data.y);
         let s_z = Series::new("z_raw", &data.z);
 
+        let s_x_raw_uncertainty = Series::new("x_raw_uncertainty", &data.x_raw_uncertainty);
+        let s_y_raw_uncertainty = Series::new("y_raw_uncertainty", &data.y_raw_uncertainty);
+        let s_z_raw_uncertainty = Series::new("z_raw_uncertainty", &data.z_raw_uncertainty);
+
         let mut df = DataFrame::new(vec![
             s_frame, s_time, /*s_conf,*/s_pose, // I will add  confidence later
             s_px, s_py, s_pz, 
             s_num, s_type, 
-            s_x, s_y, s_z
+            s_x, s_y, s_z,
+            s_x_raw_uncertainty, s_y_raw_uncertainty, s_z_raw_uncertainty,
         ])?;
 
         let file = File::create(file_path).map_err(PolarsError::from)?;
@@ -418,6 +434,9 @@ pub struct UMDCentered {
     pub x: Vec<f64>,
     pub y: Vec<f64>,
     pub z: Vec<f64>,
+    pub x_centered_uncertainty: Ved<f64>,
+    pub y_centered_uncertainty: Ved<f64>,
+    pub z_centered_uncertainty: Ved<f64>,
 }
 
 
@@ -438,10 +457,14 @@ impl UMDCentered{
             x: Vec::with_capacity(total_entries.try_into().unwrap()),
             y: Vec::with_capacity(total_entries.try_into().unwrap()),
             z: Vec::with_capacity(total_entries.try_into().unwrap()),
+            x_centered_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()),
+            y_centered_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()),
+            z_centered_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()),
         }
     }
 
-    pub fn add_point(&mut self, frame: u32, time: f32, confidence: f32, pose: bool, pose_x: f64, pose_y: f64, pose_z: f64, number: u32, types: String, x: f64, y: f64, z: f64) {
+    pub fn add_point(&mut self, frame: u32, time: f32, confidence: f32, pose: bool, pose_x: f64, pose_y: f64, pose_z: f64, 
+                     number: u32, types: String, x: f64, y: f64, z: f64, x_centered_uncertainty: f64, y_centered_uncertainty: f64, z_centered_uncertainty: f64) {
         
         self.frame.push(frame);
         self.timestamp.push(time);
@@ -449,11 +472,15 @@ impl UMDCentered{
         self.pose_x.push(pose_x);
         self.pose_y.push(pose_y);
         self.pose_z.push(pose_z);
+        
         self.coordinate_number.push(number);
         self.types.push(types);
         self.x.push(x);
         self.z.push(z);
         self.y.push(y);
+        self.x_centered_uncertainty.pushc(x_centered_uncertainty);
+        self.y_centered_uncertainty.pushc(y_centered_uncertainty);
+        self.z_centered_uncertainty.pushc(z_centered_uncertainty);
 
     }
 }
@@ -463,6 +490,7 @@ impl UMDCentered{
 // on the set of points extracted (i.e. curve fitting needs lip points which an EMA dataset
 // may not include)
 
+#[derive(Debug)]
 pub struct UMDPose{
     // like UMD and Centering, this is the same struct design
     pub frame: Vec<u32>,
@@ -473,12 +501,18 @@ pub struct UMDPose{
     pub pose_x: Vec<f64>,
     pub pose_y: Vec<f64>,
     pub pose_z: Vec<f64>,
+    pub pose_x_uncertainty: Vec<f64>, // needs implementing
+    pub pose_y_uncertainty: Vec<f64>, // needs implementing
+    pub pose_z_uncertainty: Vec<f64>, // needs implementing
 
     pub coordinate_number: Vec<u32>,
     pub types: Vec<String>, // we need to know whether or not this point was a commissure, philtrum, etc - defulat lip points can just be called "point"
     pub x: Vec<f64>,
     pub y: Vec<f64>,
     pub z: Vec<f64>,
+    pub x_rotated_uncertainty: Vec<f64>, // needs implementing
+    pub y_rotated_uncertainty: Vec<f64>, // needs implementing
+    pub z_rotated_uncertainty: Vec<f64>, // needs implementing
 }
 
 // again, quite similar to UMD and UMDCenter
@@ -494,15 +528,24 @@ impl UMDPose {
             pose_x: Vec::with_capacity(total_entries.try_into().unwrap()),
             pose_y: Vec::with_capacity(total_entries.try_into().unwrap()),
             pose_z: Vec::with_capacity(total_entries.try_into().unwrap()),
+            pose_x_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()), // needs implementing
+            pose_y_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()), // needs implementing
+            pose_z_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()), // needs implementing
+             // needs implementing
             coordinate_number: Vec::with_capacity(total_entries.try_into().unwrap()),
             types: Vec::with_capacity(total_entries.try_into().unwrap()),
             x: Vec::with_capacity(total_entries.try_into().unwrap()),
             y: Vec::with_capacity(total_entries.try_into().unwrap()),
             z: Vec::with_capacity(total_entries.try_into().unwrap()),
+            x_rotated_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()), // needs implementing
+            y_rotated_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()), // needs implementing
+            z_rotated_uncertainty: Vec::with_capacity(total_entries.try_into().unwrap()), // needs implementing
         } 
     }
 
-    pub fn add_point(&mut self, frame: u32, time: f32, confidence: f32, pose: bool, pose_x: f64, pose_y: f64, pose_z: f64, number: u32, types: String, x: f64, y: f64, z: f64) {
+    pub fn add_point(&mut self, frame: u32, time: f32, confidence: f32, pose: bool, pose_x: f64, pose_y: f64, pose_z: f64, 
+                     pose_x_uncertainty: f64, pose_y_uncertainty: f64, pose_z_uncertainty: f64, number: u32, types: String, 
+                     x: f64, y: f64, z: f64, x_rotated_uncertainty: f64, y_rotated_uncertainty: f64, z_rotated_uncertainty: f64) {
         
         self.frame.push(frame);
         self.timestamp.push(time);
@@ -510,11 +553,17 @@ impl UMDPose {
         self.pose_x.push(pose_x);
         self.pose_y.push(pose_y);
         self.pose_z.push(pose_z);
+        self.pose_x_uncertainty.push(pose_x_uncertainty); // needs implementing
+        self.pose_y_uncertainty.push(pose_y_uncertainty); // needs implementing
+        self.pose_z_uncertainty.push(pose_z_uncertainty); // needs implementing
         self.coordinate_number.push(number);
         self.types.push(types);
         self.x.push(x);
         self.z.push(z);
         self.y.push(y);
+        self.x_rotated_uncertainty.push(x_rotated_uncertainty); // needs implementing
+        self.y_rotated_uncertainty.push(y_rotated_uncertainty); // needs implementing
+        self.z_rotated_uncertainty.push(z_rotated_uncertainty); // needs implementing
 
     }
 }
